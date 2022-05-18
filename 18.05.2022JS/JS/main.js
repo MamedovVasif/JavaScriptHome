0ecbe1a746f22c7bdccd439dad5f66e7ae349337
MamedovVasif <100746134+MamedovVasif@users.noreply.github.com>
2022-05-18 03:50:31 +0100

    Add files via upload

---
+++ 18.05.2022JS/JS/main.js
@@ -0,0 +1,20 @@
+let btnMy = document.querySelector("btnMy")
+let bottomclass = document.querySelector("bottomclass")
+let frm1 = document.querySelector("frm1")
+
+
+btnMy.addEventListener("click", () => {
+    document.getElementById('log').innerHTML += '<br>Some new content!'
+
+
+});
+
+// function addTask(bottomclass,frm1) {
+//     bottomclass=0;
+//     bottomclass+= frm1;
+// }
+// btnMy.addEventListener("click", function () {
+//     bottomclass=0;
+//     bottomclass+= frm1;
+// })
+
